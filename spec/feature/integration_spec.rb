5b7d1c63fbbe4ecde689f0aa08de1d43d6a325b6
Alec Hubacher <qwertyabck@gmail.com>
2022-02-02 12:36:40 -0600

    fixed integration test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,39 +4,18 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Percy Jackson'
+    fill_in 'Author', with: 'Rick Riordan'
+    fill_in 'Price', with: 20
+    select '2013' :from => 'book_pubished_date_1i'
+    select '8' :from => 'book_pubished_date_2i'
+    select '7' :from => 'book_pubished_date_3i'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
-  end
-end
-
-RSPEC.describe 'Creating an author', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Author', with: 'J.K Rowling'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('J.K Rowling')
-  end
-end
-
-RSPEC.describe 'Creating a Price', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Price', with: '20'
-    click_on 'Create Book'
-    visit books_path
+    expect(page).to have_content('Percy Jackson')
+    expect(page).to have_content('Rick Riordan')
     expect(page).to have_content('20')
-  end
-end
+    expect(page).to have_content('2013-08-07')
 
-RSPEC.describe 'Creating a Date', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    select '2', :from => '2'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('2')
   end
 end

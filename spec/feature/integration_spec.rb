e37e57261ef9243f18e3a7e56192b99251ca570c
Alec Hubacher <qwertyabck@gmail.com>
2022-02-01 23:33:05 -0600

    added integration and unit tests for new attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,33 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('harry potter')
   end
 end
+
+RSPEC.describe 'Creating an author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'J.K Rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('J.K Rowling')
+  end
+end
+
+RSPEC.describe 'Creating a Price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: '20'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('20')
+  end
+end
+
+RSPEC.describe 'Creating a Date', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    select '2', :from => '2'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2')
+  end
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,18 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: 'J.K Rowling')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end

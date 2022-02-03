1234291591457da6ebcbca89dd6a5c6556dc9374
Alec Hubacher <qwertyabck@gmail.com>
2022-02-03 16:08:51 -0600

    testing two different integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,15 +5,20 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'Percy Jackson'
-    fill_in 'Author', with: 'Rick Riordan'
-    fill_in 'Price', with: 20
-    fill_in 'Published date', with: '2013-08-07'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('Percy Jackson')
+
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'Rick Riordan'
+    click_on 'Create Book'
+    visit books_path
     expect(page).to have_content('Rick Riordan')
-    expect(page).to have_content('20')
-    expect(page).to have_content('2013-08-07')
 
   end
 end

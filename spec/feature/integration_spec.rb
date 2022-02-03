469d5ed91705d71d1571ff68ffb2b47c06abf6f6
Alec Hubacher <qwertyabck@gmail.com>
2022-02-03 13:43:26 -0600

    fixed syntax for select drop down in integration test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'Percy Jackson'
     fill_in 'Author', with: 'Rick Riordan'
     fill_in 'Price', with: 20
-    select '2013' :from => 'book_pubished_date_1i'
-    select '8' :from => 'book_pubished_date_2i'
-    select '7' :from => 'book_pubished_date_3i'
+    select '2013', :from => 'book_pubished_date_1i'
+    select '8', :from => 'book_pubished_date_2i'
+    select '7', :from => 'book_pubished_date_3i'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('Percy Jackson')

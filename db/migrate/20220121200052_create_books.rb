edc5ba0f4fd70ade0d785baa87e7bb18aff1b8df
Alec Hubacher <qwertyabck@gmail.com>
2022-02-03 16:02:00 -0600

    date variable should be correct for integration test

--- db/migrate/20220121200052_create_books.rb
+++ db/migrate/20220121200052_create_books.rb
@@ -4,7 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :author
       t.decimal :price
-      t.date :datePublished
+      t.date :published_date
       t.timestamps
     end
   end
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'Percy Jackson'
     fill_in 'Author', with: 'Rick Riordan'
     fill_in 'Price', with: 20
-    fill_in 'DatePublished', with: '2013-08-07'
+    fill_in 'Published date', with: '2013-08-07'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('Percy Jackson')

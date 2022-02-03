20198c92668bb5da6e34b449e1bc9a0ed6da9e24
Alec Hubacher <qwertyabck@gmail.com>
2022-02-03 16:51:10 -0600

    added integration test for each attribute

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -22,3 +22,25 @@ RSpec.describe 'Creating a book', type: :feature do
 
   end
 end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: 20
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('20')
+
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Published date', with: '2013-08-07'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2013-08-07')
+
+  end
+end

ffef61fcc7ab47870be23c42511b8cc92146b97a
Alec Hubacher <qwertyabck@gmail.com>
2022-02-03 18:11:32 -0600

    changed test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,42 +5,12 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'JK'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('JK')
-
-  end
-end
-
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
     fill_in 'Author', with: 'Rick Riordan'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('Rick Riordan')
-
-  end
-end
-
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
     fill_in 'Price', with: '20'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('20')
-
-  end
-end
-
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
     fill_in 'Published date', with: '2013-10-17'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('2013-10-17')
+    expect(page).to have_content('JK')
 
   end
 end
